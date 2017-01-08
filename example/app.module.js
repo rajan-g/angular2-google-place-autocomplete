09b2005199ce5c372ceee21589c46ab89f5e3187
Rajan G <rajangmca@gmail.com>
2017-01-08 08:42:28 +0530

    ato added

--- example/app.module.js
+++ example/app.module.js
@@ -1,50 +1,29 @@
-System.register(["@angular/core", "@angular/forms", "@angular/platform-browser", "./app.component", "../directives/index"], function (exports_1, context_1) {
-    "use strict";
-    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
-    var __moduleName = context_1 && context_1.id;
-    var core_1, forms_1, platform_browser_1, app_component_1, index_1, GooglePlaceExample;
-    return {
-        setters: [
-            function (core_1_1) {
-                core_1 = core_1_1;
-            },
-            function (forms_1_1) {
-                forms_1 = forms_1_1;
-            },
-            function (platform_browser_1_1) {
-                platform_browser_1 = platform_browser_1_1;
-            },
-            function (app_component_1_1) {
-                app_component_1 = app_component_1_1;
-            },
-            function (index_1_1) {
-                index_1 = index_1_1;
-            }
-        ],
-        execute: function () {
-            GooglePlaceExample = (function () {
-                function GooglePlaceExample() {
-                }
-                return GooglePlaceExample;
-            }());
-            GooglePlaceExample = __decorate([
-                core_1.NgModule({
-                    imports: [platform_browser_1.BrowserModule, forms_1.FormsModule, index_1.GooglePlaceModule],
-                    declarations: [app_component_1.AppComponent],
-                    bootstrap: [app_component_1.AppComponent]
-                }),
-                __metadata("design:paramtypes", [])
-            ], GooglePlaceExample);
-            exports_1("GooglePlaceExample", GooglePlaceExample);
-        }
-    };
-});
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { GooglePlaceModule } from '../directives/index';
+var GooglePlaceExample = (function () {
+    function GooglePlaceExample() {
+    }
+    return GooglePlaceExample;
+}());
+GooglePlaceExample = __decorate([
+    NgModule({
+        imports: [BrowserModule, FormsModule, GooglePlaceModule],
+        declarations: [AppComponent],
+        bootstrap: [AppComponent]
+    }),
+    __metadata("design:paramtypes", [])
+], GooglePlaceExample);
+export { GooglePlaceExample };
 //# sourceMappingURL=app.module.js.map
